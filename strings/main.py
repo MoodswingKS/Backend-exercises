1015db80eeb911e28ddef5cd77caec7e4af3fde4
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-09-03 09:19:21 +0200

    Strings done

--- strings/main.py
+++ strings/main.py
@@ -3,28 +3,25 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-player_0 = 'Gullit'
-player_1 = 'Van Basten'
+player_0 = 'Ruud Gullit'
+player_1 = 'Marco van Basten'
 
 goal_0 = 32
 goal_1 = 54
 
 scorers = player_0 + ' ' + str(goal_0) + ', ' + player_1 + ' ' + str(goal_1)
-report = f'{player_0} scored in the {goal_0}nd minute\n{player_1} scored in the {goal_1}th minute' 
-print(scorers)
-print(report)
+report = f'{player_0} scored in the {goal_0}nd minute\n{player_1} scored in the {goal_1}th minute'
 
 player = 'Jan Wouters'
-first_name = player[:3]
-last_name = player[-7:]
+first_name = player.split()[0]
+last_name = player.split()[-1]
 last_name_len = len(last_name)
-
 
 name_short = first_name[0] + '. ' + last_name
 print(name_short)
 
-chant = (first_name + '! ') * len(first_name)
-good_chant = chant+'!' != 3
+chant = (first_name + '! ') * (len(first_name) - 1) + first_name + '!'
+good_chant = chant != ' '
 print(chant)
 print(good_chant)
 
